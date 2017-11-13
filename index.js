eef6daa00af6e304e7d87240f6a35874ed9889af
Chris Pang <chris_pang429@outlook.com>
2017-11-13 23:34:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function Scooter(year, color, model) {
-  this.year = year 
+  this.year = year
   this.color = color
-  this.model = model 
+  this.model = model
 }
+
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
